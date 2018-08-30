bc2be7811c5ff6a5f16557f7d958c7335964e662
Noah Kim <noahbkim@gmail.com>
2018-08-30 15:49:53 -0700

    Touch up templates

--- home/models.py
+++ home/models.py
@@ -23,4 +23,4 @@ class Subject(models.Model):
     semester = models.CharField(max_length=5)  # Formatted YYYYS where S can be 1 or 2
     birthday = models.DateField()
     sex = models.BooleanField()
-    prosopagnosia = models.PositiveSmallIntegerField(choices=((item, item.value for item in PROSOPAGNOSIA)))
+    prosopagnosia = models.PositiveSmallIntegerField(choices=((item, item.value) for item in PROSOPAGNOSIA))
--- home/views.py
+++ home/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    """Render the home page of the website."""
+
+    return render(request, "home/index.html")
